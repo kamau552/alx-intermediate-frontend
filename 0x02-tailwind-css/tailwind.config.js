2e70b90aa842878c10b1e58688afec198423b528
Njeri Kamau <faithkamau730@gmail.com>
2025-05-18 15:55:14 +0300

    nested

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./*.html",
-    "./src/**/*.{html,js}"
-  ],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
